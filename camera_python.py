0b5f55800c9f833233c4eb19d0b12fa4afda150f
Edgar8a30 <83721247+Edgar8a30@users.noreply.github.com>
2021-05-05 12:14:28 -0500

    Add files via upload

---
+++ camera_python.py
@@ -0,0 +1,46 @@
+import cv2
+import time
+import argparse
+
+
+if __name__ == '__main__':
+    script_start_time = time.time()
+
+    parser = argparse.ArgumentParser(description='Camera visualization')
+
+    ### Positional arguments
+    parser.add_argument('-i', '--cameraSource', default=0, help="Introduce number or camera path, default is 0 (default cam)")
+
+    
+    args = vars(parser.parse_args())
+
+
+    cap = cv2.VideoCapture(args["cameraSource"]) #0 local o primary camera
+    while cap.isOpened():
+        
+        #BGR image feed from camera
+        success,img = cap.read()
+        #BGR to grayscale
+        img_gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        #cv2.Gaussian...
+        if not success:
+            break
+        if img is None:
+            break
+
+        
+        cv2.imshow("Output", img)
+
+        k = cv2.waitKey(10)
+        if k==27:
+            break
+
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+    print('Script took %f seconds.' % (time.time() - script_start_time))
+
+
+
